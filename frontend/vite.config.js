e75a54f057936dc873a35541959f0b686b3d0ff0
shivamrao7 <shivamrao077@gmail.com>
2025-09-20 16:33:46 +0530

    update
    update

---
+++ frontend/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: '../backend/static',
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      '/generate_keywords': 'http://localhost:8000',
+    }
+  }
+})
